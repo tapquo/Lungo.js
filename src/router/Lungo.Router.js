37d371640e8694f5029b00b2438b22e4f8a1ef75
Javier Jiménez Villar <javi.jimenez.villar@gmail.com>
2011-11-07 13:09:22 +0100

    New feature: navigation via data-target attribute
    In Release 1.0.1 will be discontinued navigation via <class>

--- src/router/Lungo.Router.js
+++ src/router/Lungo.Router.js
@@ -24,12 +24,15 @@ LUNGO.Router = (function(lng, undefined) {
      * @param {string} Id of the <section>
      */
     var section = function(section_id) {
-        section_id = (section_id.indexOf('#')) ? '#' + section_id : section_id;
+        var section_id = (section_id.indexOf('#')) ? '#' + section_id : section_id;
+        var target = 'section' + section_id;
 
-        lng.Dom.query(_getHistoryCurrent()).removeClass(CSS_CLASSES.SHOW).addClass(CSS_CLASSES.HIDE);
-        lng.Dom.query(section_id).addClass(CSS_CLASSES.SHOW);
+        if (_existsTarget(target)) {
+            lng.Dom.query(_getHistoryCurrent()).removeClass(CSS_CLASSES.SHOW).addClass(CSS_CLASSES.HIDE);
+            lng.Dom.query(section_id).addClass(CSS_CLASSES.SHOW);
 
-        lng.Router.History.add(section_id);
+            lng.Router.History.add(section_id);
+        }
     };
 
     /**
@@ -41,7 +44,11 @@ LUNGO.Router = (function(lng, undefined) {
      * @param {string} <article> Id
      */
     var article = function(section_id, article_id) {
-        lng.View.Article.show(section_id, article_id);
+        var target = section_id + ' article' + article_id;
+
+        if (_existsTarget(target)) {
+            lng.View.Article.show(section_id, article_id);
+        }
     };
 
     /**
@@ -55,6 +62,17 @@ LUNGO.Router = (function(lng, undefined) {
 
         lng.Dom.query(_getHistoryCurrent()).removeClass(CSS_CLASSES.HIDE).addClass(CSS_CLASSES.SHOW);
     };
+
+    var _existsTarget = function(target) {
+        var exists = false;
+
+        if ($(target).length > 0) {
+            exists = true;
+        } else {
+            lng.Core.log(3, 'Lungo.Router ERROR: The target ' + target + ' does not exists.');
+        }
+        return exists;
+    }
 
     var _getHistoryCurrent = function() {
         return lng.Router.History.current();
