50ceb66ac908ae28f724cc2d29d51bb1fb4d03d5
@soyjavi <javi.jimenez.villar@gmail.com>
2011-11-25 20:14:19 +0100

    Clean methods and change Tabs for WhiteSpaces (4)

--- src/Lungo.Events.js
+++ src/Lungo.Events.js
@@ -19,11 +19,11 @@ LUNGO.Events = (function(lng, undefined) {
             TAP: 'tap',
             DOUBLE_TAP: 'doubletap',
             ORIENTATION_CHANGE: 'orientationchange',
-			SWIPE:'swipe',
-			SWIPE_LEFT:'swipeLeft',
-			SWIPE_RIGHT:'swipeRight',
-			SWIPE_UP: 'swipeUp',
-			SWIPE_DOWN:'swipeDown'			
+            SWIPE:'swipe',
+            SWIPE_LEFT:'swipeLeft',
+            SWIPE_RIGHT:'swipeRight',
+            SWIPE_UP: 'swipeUp',
+            SWIPE_DOWN:'swipeDown'
         },
         desktop: {
             TOUCH_START: 'click',
@@ -45,8 +45,8 @@ LUNGO.Events = (function(lng, undefined) {
      * @return {string} Touch event based on the current environment
      */
     var get = function(eventName) {
-		var current_environment = lng.Environment.current();
-		var current_events = EVENTS[current_environment];
+        var current_environment = lng.Environment.current();
+        var current_events = EVENTS[current_environment];
         return current_events[eventName];
     };
 
