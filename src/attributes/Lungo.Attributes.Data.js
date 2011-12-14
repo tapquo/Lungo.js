10b99d895b7c4388b1bcde5f9e83b9f117a4f6fa
@soyjavi <javi.jimenez.villar@gmail.com>
2011-12-14 16:07:11 +0100

    Refactor in Data-Attributes file

--- src/attributes/Lungo.Attributes.Data.js
+++ src/attributes/Lungo.Attributes.Data.js
@@ -1,6 +1,6 @@
-/** 
+/**
  * Object with data-attributes (HTML5) with a special <markup>
- * 
+ *
  * @namespace LUNGO.Attributes
  * @class Data
  *
@@ -9,24 +9,29 @@
  */
 
 LUNGO.Attributes.Data = {
-     search: {
-         tag: 'search',
-         selector: '.list',
-         html: '<li class="search {{value}}"><input type="search" placeholder="Search..."><a href="#" class="button" data-icon="search"></a></li>'
-     },
-     icon: {
-         tag: 'icon',
-         selector: '*',
-         html: '<span class="icon {{value}}"></span>'
-     },
-     title: {
-         tag: 'title',
-         selector: 'header, footer',
-         html: '<h1 class="title">{{value}}</h1>'
-     },
-     back: {
-         tag: 'back',
-         selector: 'header, footer',
-         html: '<a href="#back" data-target="section" class="back onleft button icon {{value}}"></a>'
-     }
+    search: {
+        tag: 'search',
+        selector: '.list',
+        html: '<li class="search {{value}}"><input type="search" placeholder="Search..."><a href="#" class="button" data-icon="search"></a></li>'
+    },
+    icon: {
+        tag: 'icon',
+        selector: '*',
+        html: '<span class="icon {{value}}"></span>'
+    },
+    image: {
+        tag: 'image',
+        selector: '*',
+        html: '<img src="{{value}}" class="icon" />'
+    },
+    title: {
+        tag: 'title',
+        selector: 'header, footer',
+        html: '<h1 class="title">{{value}}</h1>'
+    },
+    back: {
+        tag: 'back',
+        selector: 'header, footer',
+        html: '<a href="#back" data-target="section" class="back onleft button icon {{value}}"></a>'
+    }
 };
