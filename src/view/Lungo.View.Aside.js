2574a9b07c26bd437e3e2ec1a9d20b5e9ea2b52f
@soyjavi <javi.jimenez.villar@gmail.com>
2011-12-16 14:28:48 +0100

    Refactor class Lungo.View.Aside

--- src/view/Lungo.View.Aside.js
+++ src/view/Lungo.View.Aside.js
@@ -2,7 +2,7 @@
  * Initialize the <articles> layout of a certain <section>
  *
  * @namespace LUNGO.View
- * @class Article
+ * @class Aside
  *
  * @author Javier Jimenez Villar <javi@tapquo.com> || @soyjavi
  * @author Guillermo Pascual <pasku@tapquo.com> || @pasku1
@@ -11,29 +11,8 @@
 LUNGO.View.Aside = (function(lng, undefined) {
 
     var toggle = function(section_id) {
-        if (_isVisible(section_id)) {
-            _hide(section_id);
-        } else {
-            _show(section_id);
-        }
-    };
-
-    var _show = function(section_id) {
-        lng.Dom.query(section_id + ' aside').addClass('show');
-
         var articles = lng.Dom.query(section_id + ' article');
         articles.toggleClass('aside');
-    };
-
-    var _hide = function(section_id) {
-        var articles = lng.Dom.query(section_id + ' article');
-        articles.toggleClass('aside');
-    };
-
-    var _isVisible = function(section_id) {
-        var isVisible = lng.Dom.query(section_id + ' aside').hasClass('show');
-
-        return isVisible;
     };
 
     return {
