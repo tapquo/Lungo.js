2fbc7840f5af9cfd8a4c09a6d422c05f8c6f207a
@soyjavi <javi.jimenez.villar@gmail.com>
2011-12-21 09:05:55 +0100

    Merge from branch 1.0.4

--- vendor/node-jasmine-dom/examples/tests/spec/example-dom_spec.js
+++
@@ -1,9 +0,0 @@
-describe("Example functions that update the DOM", function(){
-	it("Should add two numbers", function(){
-
-		EXAMPLES.domAdd('add_test_one_x', 'add_test_one_y', 'add_test_one_result');
-		var result = jQuery('#add_test_one_result').html();
-
-		expect(result).toEqual('7');
-	});
-});
