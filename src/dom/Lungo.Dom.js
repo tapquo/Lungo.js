8679d747e21c48a60c8a19c27f181b992689ac03
@soyjavi <javi.jimenez.villar@gmail.com>
2012-01-08 10:06:37 +0100

    New DOMHandler: QuoJS

--- src/dom/Lungo.Dom.js
+++ src/dom/Lungo.Dom.js
@@ -1,6 +1,6 @@
-/** 
+/**
  * LungoJS Dom Handler
- * 
+ *
  * @namespace LUNGO
  * @class Dom
  *
@@ -8,7 +8,7 @@
  * @author Guillermo Pascual <pasku@tapquo.com> || @pasku1
  */
 
-LUNGO.Dom = (function(lng, $, undefined) {
+LUNGO.Dom = (function(lng, $$, undefined) {
 
     /**
      * Add an event listener
@@ -19,11 +19,11 @@ LUNGO.Dom = (function(lng, $, undefined) {
      * @return {Object} Zepto <element> instance
      */
     var query = function(selector) {
-        return $(selector);
+        return $$(selector);
     };
 
     return {
         query: query
     };
 
-})(LUNGO, Zepto);
+})(LUNGO, Quo);
