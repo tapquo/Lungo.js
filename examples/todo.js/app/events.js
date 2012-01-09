8da410299f65f6428970d015660b2377be55eb87
@soyjavi <javi.jimenez.villar@gmail.com>
2012-01-09 12:44:56 +0100

    New <markup/> for examples

--- examples/todo.js/app/events.js
+++ examples/todo.js/app/events.js
@@ -9,9 +9,9 @@ App.Events = (function(lng, undefined) {
     //Create new todo
     lng.Dom.Event.live('#btnNewTodo', 'TAP', function(event) {
 
-        var name = lng.Dom.query('#txtNewName');
-        var description = lng.Dom.query('#txtNewDescription');
-        var type = lng.Dom.query('#txtNewType');
+        var name = lng.dom('#txtNewName');
+        var description = lng.dom('#txtNewDescription');
+        var type = lng.dom('#txtNewType');
 
         App.Data.insertTodo({
             name: name.val(),
@@ -28,7 +28,7 @@ App.Events = (function(lng, undefined) {
 
     //View ToDo
     lng.Dom.Event.live('#done li, #pending li', 'TAP', function(event) {
-        var todo_id = lng.Dom.query(this).attr('id');
+        var todo_id = lng.dom(this).attr('id');
         App.View.todo(todo_id)
     });
 
@@ -43,9 +43,9 @@ App.Events = (function(lng, undefined) {
     //Update ToDo
     lng.Dom.Event.live('#btnUpdateTodo', 'TAP', function(event) {
         var current_todo = lng.Data.Cache.get('current_todo');
-        var name = lng.Dom.query('#txtEditName');
-        var description = lng.Dom.query('#txtEditDescription');
-        var type = lng.Dom.query('#txtNewType');
+        var name = lng.dom('#txtEditName');
+        var description = lng.dom('#txtEditDescription');
+        var type = lng.dom('#txtNewType');
 
         App.Data.updateTodo(current_todo.id, {
             name: name.val(),
