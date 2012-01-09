efd1ff575e26d631e10986cea53dbf1f02160210
@soyjavi <javi.jimenez.villar@gmail.com>
2012-01-09 14:52:36 +0100

    New <markup/> for examples

--- examples/kitchen-sink/app/services.js
+++ examples/kitchen-sink/app/services.js
@@ -41,6 +41,44 @@ App.Services = (function(lng, App, undefined) {
         lng.View.Template.List.create(parameters);
     };
 
+    var testService = function() {
+        lng.Service.Settings.timeout = 2500;
+        lng.Service.Settings.dataType = 'json';
+        lng.Service.Settings.error = function() {
+            console.error('Timeout exceed (500ms):', arguments);
+        };
+
+        var url = 'http://www.panoramio.com/map/get_panoramas.php';
+        var parameters = {
+            set: 'public',
+            from: 0,
+            to: 3,
+            minx: -180,
+            miny: -90,
+            maxx: 180,
+            maxy: 90,
+            size: 'medium',
+            mapfilter: true
+        };
+
+        lng.Service.get(url, parameters,
+            function(response) {
+                console.error('GET', response);
+            }
+        );
+
+        lng.Service.json(url, parameters,
+            function(response) {
+                console.error('JSON', response);
+            }
+        );
+
+        lng.Service.Settings.async = false;
+        var response = lng.Service.json(url, parameters);
+        console.error('SYNC', response);
+    };
+
+    testService();
     mockProfiles();
 
     return {
