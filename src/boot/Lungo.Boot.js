599828d3e49282296de4254dc371dd054275d0ac
@soyjavi <javi.jimenez.villar@gmail.com>
2012-01-19 11:28:42 +0100

    Fixed issue when response it's null

--- src/boot/Lungo.Boot.js
+++ src/boot/Lungo.Boot.js
@@ -11,12 +11,12 @@
 LUNGO.Boot = (function(lng, undefined) {
 
     return function() {
-        lng.Boot.Stats.start();
         lng.Boot.Layout.start();
         lng.Boot.Events.start();
         lng.Boot.Data.start();
         lng.Boot.Section.start();
         lng.Boot.Article.start();
+        lng.Boot.Stats.start();
     };
 
 })(LUNGO);
