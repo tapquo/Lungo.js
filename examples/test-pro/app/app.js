e39b21b0afd608c1845d5064b6e68542b67e51bd
@soyjavi <javi.jimenez.villar@gmail.com>
2012-02-02 20:30:54 +0100

    New example project for new features

---
+++ examples/test-pro/app/app.js
@@ -0,0 +1,28 @@
+var App = (function(lng, undefined) {
+
+
+    lng.App.init({
+        name: 'Kitchen Sink',
+        version: '1.1',
+        sections: [/*
+            'local.html',
+            'http://examples.tapquo.com/remote.html'*/
+        ]
+    });
+
+    var _getEnvironmentFromQuoJS = (function() {
+        var environment = lng.Core.environment();
+        if (environment.isMobile) {
+           // alert('Your phone is ' + environment.os.name + ' (' + environment.os.version + ')');
+        }
+    })();
+
+    lng.View.Element.count('#btn-one', 7);
+    lng.View.Element.count('#btn-three', 0);
+    lng.View.Element.count('#aside-one', 17);
+
+    return {
+
+    };
+
+})(LUNGO);
