0f268633654925346091cf0f6405ecb99fb09265
@soyjavi <javi.jimenez.villar@gmail.com>
2012-02-12 12:27:25 +0100

    New namespace CONSTANTS

---
+++ src/Lungo.Constants.js
@@ -0,0 +1,22 @@
+/**
+ * Object with data-attributes (HTML5) with a special <markup>
+ *
+ * @namespace LUNGO
+ * @class Constants
+ *
+ * @author Javier Jimenez Villar <javi@tapquo.com> || @soyjavi
+ * @author Guillermo Pascual <pasku@tapquo.com> || @pasku1
+ */
+
+LUNGO.Constants = {
+
+    ELEMENT = {
+        SECTION: 'section',
+        ARTICLE: 'article',
+        ASIDE: 'aside'
+    };
+
+    CLASSES = {
+        ACTIVE: 'active'
+    };
+};
