afa296f2f06734dd90d9fcae56318687998f57d2
@soyjavi <javi.jimenez.villar@gmail.com>
2012-02-12 12:07:47 +0100

    Fallback for non active input in Android OS (<4)

--- src/Lungo.Fallback.js
+++ src/Lungo.Fallback.js
@@ -18,11 +18,30 @@ LUNGO.Fallback = (function(lng, undefined) {
         }
     };
 
+    var androidInputs = function(article_id, active) {
+        environment = lng.Core.environment();
+        if (environment.isMobile && environment.os.name === 'android' && environment.os.version < '4') {
+            var selector = article_id + ' input, ' + article_id + ' textarea, ' + article_id + ' select';
+            var input_elements = lng.dom(selector);
+            for (var i = 0, len = input_elements.length; i < len; i++) {
+                (active) ? _enableAndroidInput(input_elements[i]) : _disableAndroidInput(input_elements[i]);
+            }
+        }
+    };
+
     var positionFixed = function(sections) {
     	environment = lng.Core.environment();
         if (environment.isMobile && environment.os.name === 'ios' && environment.os.version >= '4.2') {
             sections.style('position', 'fixed');
         }
+    };
+
+    var _enableAndroidInput = function(input) {
+        input.removeAttribute('disabled');
+    };
+
+    var _disableAndroidInput = function(input) {
+        input.setAttribute('disabled', 'disabled');
     };
 
     var _addClassActiveToButton = function(element) {
@@ -35,6 +54,7 @@ LUNGO.Fallback = (function(lng, undefined) {
 
     return {
     	androidButtons: androidButtons,
+        androidInputs: androidInputs,
     	positionFixed: positionFixed
     }
 
