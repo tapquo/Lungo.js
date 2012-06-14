5c0fbef695b9b618637249983cc82dbc2929aad9
@soyjavi <javi.jimenez.villar@gmail.com>
2012-06-14 15:21:19 +0200

    DOMHandler Perfomance: Aside Element

--- src/view/Lungo.View.Aside.js
+++ src/view/Lungo.View.Aside.js
@@ -22,13 +22,12 @@ LUNGO.View.Aside = (function(lng, undefined) {
      * @param  {string} Section id
      * @param  {string} Aside id
      */
-    var show = function(section_id, aside_id) {
-        var aside = lng.dom(ELEMENT.ASIDE + aside_id);
-        var aside_class = _asideClass(aside);
+    var show = function(section_id, aside) {
+        var aside_stylesheet = _asideStylesheet(aside);
         var section = lng.dom(ELEMENT.SECTION + section_id);
 
         aside.addClass(CLASS.CURRENT);
-        section.addClass(aside_class).addClass(CLASS.ASIDE);
+        section.addClass(aside_stylesheet).addClass(CLASS.ASIDE);
     };
 
     /**
@@ -39,26 +38,25 @@ LUNGO.View.Aside = (function(lng, undefined) {
      * @param  {string} Element query selector
      * @param  {string} Value for counter
      */
-    var hide = function(section_id, aside_id) {
-        var aside = lng.dom(ELEMENT.ASIDE + aside_id);
-        var aside_class = _asideClass(aside);
+    var hide = function(section_id, aside) {
+        var aside_stylesheet = _asideStylesheet(aside);
         var section = lng.dom(ELEMENT.SECTION + section_id);
 
-        section.removeClass(CLASS.ASIDE).removeClass(aside_class);
+        section.removeClass(CLASS.ASIDE).removeClass(aside_stylesheet);
 
         setTimeout(function() {
             aside.removeClass(CLASS.CURRENT);
         }, 300);
     };
 
-    var _asideClass = function(aside) {
-        var aside_class = aside.attr(ATTRIBUTE.CLASS);
+    var _asideStylesheet = function(aside) {
+        var aside_stylesheet = aside.attr(ATTRIBUTE.CLASS);
         var classes = '';
 
         //@todo: Refactor
-        if (aside_class) {
-            classes += (aside_class.indexOf(CLASS.RIGHT) > -1) ? CLASS.RIGHT : '';
-            classes += (aside_class.indexOf(CLASS.SMALL) > -1) ? CLASS.SMALL : '';
+        if (aside_stylesheet) {
+            classes += (aside_stylesheet.indexOf(CLASS.RIGHT) > -1) ? CLASS.RIGHT : '';
+            classes += (aside_stylesheet.indexOf(CLASS.SMALL) > -1) ? CLASS.SMALL : '';
         }
 
         return classes;
