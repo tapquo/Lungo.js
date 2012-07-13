308a754294fb5dbd4a7201c3656a1b588e223fa2
@soyjavi <javi.jimenez.villar@gmail.com>
2012-07-13 21:45:41 +0200

    Changes on EVENTS

--- vendor/build.sh
+++ vendor/build.sh
@@ -20,7 +20,7 @@ echo -e "\033[0m"============================ LUNGO COMPILER ===================
     #Main
     DIR=$LUNGO_SOURCES$LUNGO_NAMESPACE
     echo -e "\033[33m  [DIR]: "$LUNGO_SOURCES
-    FILES=(js Constants.js App.js Core.js Dom.js Service.js Element.js Fallback.js)
+    FILES=(js Constants.js App.js Core.js Dom.js Service.js Element.js Fallback.js Events.js)
     for file in "${FILES[@]}"
     do
         FILES_TO_COMPILE=$FILES_TO_COMPILE" --js "$DIR$file
