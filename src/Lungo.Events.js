79860f5672da616f340c1ccc53ce3ad9b501a91f
soyjavi <javi@tapquo.com>
2012-09-10 16:35:45 +0200

    Redefine pattern of event subscribe

--- src/Lungo.Events.js
+++ src/Lungo.Events.js
@@ -13,11 +13,11 @@ Lungo.Events = (function(lng, undefined) {
 
     var init = function(events) {
         for (event in events) {
-            var index_of = event.lastIndexOf(SPACE_CHAR);
-            if (index_of > 0) {
-                var element = event.substring(0, index_of);
-                var event_name = event.substring(index_of + 1);
 
+            var index_of = event.indexOf(SPACE_CHAR);
+            if (index_of > 0) {
+                var event_name = event.substring(0, index_of);
+                var element = event.substring(index_of + 1);
                 lng.dom(element).on(event_name, events[event]);
             }
         }
