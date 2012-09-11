9b751aeb9ddd5b9cdd5eddacb1ef212a22f481d2
soyjavi <javi@tapquo.com>
2012-09-11 16:03:57 +0200

    Correct order for events subscriber

--- examples/test/app/events.js
+++ examples/test/app/events.js
@@ -1,9 +1,9 @@
 
 Lungo.Events.init({
-    'section#a article a tap': function(){
+    'tap section#a article a': function(){
         Lungo.Notification.loading('hole', '1', 'user');
     },
-    'section#sec-1 header #btn-toggle-loading tap': App.View.toggleLoading,
-    'section#sec-2 load': App.View.toggleLoading
+    'tap section#sec-1 header #btn-toggle-loading': App.View.toggleLoading,
+    'load section#sec-2 load': App.View.toggleLoading
 });
 
