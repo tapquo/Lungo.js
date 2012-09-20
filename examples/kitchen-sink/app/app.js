acc765abada7044d055ddce8fb08bdd33bb2d2ec
soyjavi <javi@tapquo.com>
2012-09-20 15:30:06 +0200

    Minimal changes in kitchensink.app

--- examples/kitchen-sink/app/app.js
+++ examples/kitchen-sink/app/app.js
@@ -3,7 +3,7 @@ var App = (function(lng, undefined) {
     var _getEnvironmentFromQuoJS = (function() {
         var environment = lng.Core.environment();
         if (environment.isMobile) {
-            alert('Your phone is ' + environment.os.name + ' (' + environment.os.version + ')');
+            alert('Your Device is ' + environment.os.name + ' (' + environment.os.version + ')');
         }
     })();
 
