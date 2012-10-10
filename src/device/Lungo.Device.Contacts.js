89ffcb14eb91a4f3be65fc5174ae6c1c41cc9778
soyjavi <javi@tapquo.com>
2012-10-10 12:15:08 +0200

    New namespace Lungo.Device

---
+++ src/device/Lungo.Device.Contacts.js
@@ -0,0 +1,18 @@
+/**
+ * Contacts API (HTML5 Feature)
+ * Pending to final SPEC, now it's a Phonegap Wrapper
+ * http://www.w3.org/TR/contacts-api/
+ *
+ * @namespace LUNGO.Device
+ * @class Contacts
+ *
+ * @author Javier Jimenez Villar <javi@tapquo.com> || @soyjavi
+ */
+
+LUNGO.Device.Contacts = (function(lng, undefined) {
+
+    return {
+
+    };
+
+})(LUNGO);
