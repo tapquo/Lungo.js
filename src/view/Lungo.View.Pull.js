13dc6da47c2d25c98fa791ab03bc1326664a7f4c
ina <ina@tapquo.com>
2012-10-19 10:55:14 +0200

    New Namespaces in View

---
+++ src/view/Lungo.View.Pull.js
@@ -0,0 +1,11 @@
+/**
+ * Initialize the <articles> layout of a certain <section>
+ *
+ * @namespace Lungo.View
+ * @class Element
+ *
+ * @author Ignacio Olalde <ina@tapquo.com> || @piniphone
+ * @author Javier Jimenez Villar <javi@tapquo.com> || @soyjavi
+ */
+
+Lungo.View.Pull = {}
