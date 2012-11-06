99dd085caeb63e2018803f02896ff566c5cee1a5
soyjavi <javi.jimenez.villar@gmail.com>
2012-11-06 16:52:46 +0100

    Rename variable

--- kitchen-sink/app/events.pull.js
+++ kitchen-sink/app/events.pull.js
@@ -1,9 +1,9 @@
-var pull = new Lungo.Element.Pull('section#pull article', {
+var pull_example = new Lungo.Element.Pull('section#pull article', {
     onPull: "Pull down to refresh",
     onRelease: "Release to get new data",
     onRefresh: "Refreshing...",
     callback: function() {
         alert("Pull & Refresh completed!");
-        pull.hide();
+        pull_example.hide();
     }
 });
