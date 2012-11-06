507cb95dd19a1adfef5d0fa79d4a93fbdd6bd630
soyjavi <javi.jimenez.villar@gmail.com>
2012-11-06 16:52:57 +0100

    Final example of Carousel

--- kitchen-sink/app/events.carousel.js
+++ kitchen-sink/app/events.carousel.js
@@ -1,1 +1,8 @@
-Lungo.Element.Carousel($$('[data-control=carousel]')[0]);
+var carousel_example = Lungo.Element.Carousel($$('[data-control=carousel]')[0], function(index, element) {
+    Lungo.dom("section#carousel .subtitle span").html(index + 1);
+});
+
+Lungo.Events.init({
+    'tap section#carousel > header [data-direction=left]':  carousel_example.prev,
+    'tap section#carousel > header [data-direction=right]': carousel_example.next
+});
