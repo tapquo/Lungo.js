c6e021824ea5581f8f3f3744b6f1a7cbb8a5042a
soyjavi <javi.jimenez.villar@gmail.com>
2012-11-06 16:15:35 +0100

    Feature "Environment"

--- kitchen-sink/app/events.trigger.js
+++ kitchen-sink/app/events.trigger.js
@@ -3,3 +3,8 @@ Lungo.Events.init({
     'load section#layoutevents': App.triggerCapture,
     'unload section#layoutevents': App.triggerCapture
 });
+
+Lungo.ready(function() {
+    Lungo.View.Aside.show('features');
+    App.environment();
+});
