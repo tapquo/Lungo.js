f18ba6f3c92402d2466a6d7ffe748b9da45899a3
soyjavi <javi.jimenez.villar@gmail.com>
2012-12-19 09:44:28 +0100

    Change kitchen-sink

--- kitchen-sink/app/events.trigger.js
+++ kitchen-sink/app/events.trigger.js
@@ -13,20 +13,20 @@ Lungo.ready(function() {
     // Lungo.Notification.show("Please wait", "user", 2, function(){ alert(1); });
     // Lungo.Notification.error('Lorem ipsum dolor sit amet, consectetur adipisicing.', "tap to continue", 'cancel');
     // Lungo.Notification.success('Lorem ipsum dolor sit amet, consectetur adipisicing.', "tap to continue", 'cancel', 2, function(){alert(1)});
-    Lungo.Notification.confirm({
-        icon: 'user',
-        title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo amet nulla dolorum hic eum debitis dolorem expedita? Commodi molestiae tempora totam explicabo sed deserunt cum iusto eos perspiciatis ea in.',
-        accept: {
-            icon: 'checkmark',
-            label: 'Accept',
-            callback: function(){ alert("Yes!"); }
-        },
-        cancel: {
-            icon: 'close',
-            label: 'Cancel',
-            callback: function(){ alert("No!"); }
-        }
-    });
+    // Lungo.Notification.confirm({
+    //     icon: 'user',
+    //     title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+    //     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo amet nulla dolorum hic eum debitis dolorem expedita? Commodi molestiae tempora totam explicabo sed deserunt cum iusto eos perspiciatis ea in.',
+    //     accept: {
+    //         icon: 'checkmark',
+    //         label: 'Accept',
+    //         callback: function(){ alert("Yes!"); }
+    //     },
+    //     cancel: {
+    //         icon: 'close',
+    //         label: 'Cancel',
+    //         callback: function(){ alert("No!"); }
+    //     }
+    // });
 
 });
