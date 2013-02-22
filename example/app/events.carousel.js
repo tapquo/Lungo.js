60e8077701034482431c63aa0b73056f3d6fa08c
Javi Jimenez Villar <javi@tapquo.com>
2013-02-22 13:04:04 +0100

    Changes in Kitchensink

--- example/app/events.carousel.js
+++
@@ -1,8 +0,0 @@
-var carousel_example = Lungo.Element.Carousel($$('[data-control=carousel]')[0], function(index, element) {
-    Lungo.dom("section#carousel .title span").html(index + 1);
-});
-
-Lungo.Events.init({
-    'tap section#carousel > header [data-direction=left]':  carousel_example.prev,
-    'tap section#carousel > header [data-direction=right]': carousel_example.next
-});
